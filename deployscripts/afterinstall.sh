86c228c56d1f8490f8a0a3f257a1950c523f5434
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-07-22 15:06:18 +0900

    afterinstallの挙動確認

--- deployscripts/afterinstall.sh
+++ deployscripts/afterinstall.sh
@@ -2,6 +2,8 @@
 
 PRODUCTION_DIR=/srv/app
 
+echo $HOME > /tmp/home
+
 cd ${PRODUCTION_DIR}
 /usr/local/rbenv/shims/bundle install
 /usr/local/rbenv/shims/rake assets:precompile RAILS_ENV=production
