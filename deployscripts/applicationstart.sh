53a488cb521ae18ece6a41dc792d911a394172e4
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-07-22 15:56:49 +0900

    applicationstart処理を追加

---
+++ deployscripts/applicationstart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+readonly PRODUCTION_DIR=/srv/app
+readonly UNICORN_MASTER_ID=`ps aux | grep unicorn_rails\ master | grep -v grep | awk '{ print $2 }'`
+
+if [ -n "$UNICORN_MASTER_ID" ]; then
+  kill -9 $UNICORN_MASTER_ID
+fi
+
+/usr/local/rbenv/shims/bundle exec unicorn_rails -c ${PRODUCTION_DIR}/config/unicorn.conf.rb -D -E production
+nginx -s reload
