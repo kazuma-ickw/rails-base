634887e24a0682a41386b91522d9ba812b75c1c5
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-07-23 14:56:00 +0900

    bundle installのinstall先pathを追加

--- deployscripts/afterinstall.sh
+++ deployscripts/afterinstall.sh
@@ -5,5 +5,5 @@ readonly PRODUCTION_DIR=/srv/app
 env > /tmp/home
 
 cd ${PRODUCTION_DIR}
-/usr/local/rbenv/shims/bundle install
+/usr/local/rbenv/shims/bundle install --path=vendor/bundle
 /usr/local/rbenv/shims/rake assets:precompile RAILS_ENV=production
