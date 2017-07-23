38a75ed422765ad0ed6a03756a9f012eb4e973d4
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-07-23 14:56:16 +0900

    実行ディレクトリに移動する処理を追加

--- deployscripts/applicationstart.sh
+++ deployscripts/applicationstart.sh
@@ -7,5 +7,6 @@ if [ -n "$UNICORN_MASTER_ID" ]; then
   kill -9 $UNICORN_MASTER_ID
 fi
 
+cd ${PRODUCTION_DIR}
 /usr/local/rbenv/shims/bundle exec unicorn_rails -c ${PRODUCTION_DIR}/config/unicorn.rb -D -E production > /tmp/test.log
 nginx -s reload
