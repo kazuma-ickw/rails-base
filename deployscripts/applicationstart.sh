6571a68cf53b34849f9452f8426f793cfc27dd47
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-07-30 19:40:05 +0900

    unicorn, nginxの稼働状態に合わせて処理を分岐する

--- deployscripts/applicationstart.sh
+++ deployscripts/applicationstart.sh
@@ -3,10 +3,18 @@
 readonly PRODUCTION_DIR=/srv/app
 readonly UNICORN_MASTER_ID=`ps aux | grep unicorn_rails\ master | grep -v grep | awk '{ print $2 }'`
 
+cd ${PRODUCTION_DIR}
+
 if [ -n "$UNICORN_MASTER_ID" ]; then
   kill -9 $UNICORN_MASTER_ID
+  /usr/local/rbenv/shims/bundle exec unicorn_rails -c ${PRODUCTION_DIR}/config/unicorn.rb -D -E production > /tmp/test.log
+else
+  /usr/local/rbenv/shims/bundle exec unicorn_rails -c ${PRODUCTION_DIR}/config/unicorn.rb -D -E production > /tmp/test.log
 fi
 
-cd ${PRODUCTION_DIR}
-/usr/local/rbenv/shims/bundle exec unicorn_rails -c ${PRODUCTION_DIR}/config/unicorn.rb -D -E production > /tmp/test.log
-nginx -s reload
+readonly NGINX_MASTER_ID=`ps aux | grep nginx:\ master | grep -v grep | awk '{ print $2 }'`
+if [ -n "$UNICORN_MASTER_ID" ]; then
+  nginx -s reload
+else
+  nginx
+fi
