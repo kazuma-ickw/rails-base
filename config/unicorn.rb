0834e9c75abaf38453a0bd96b9badcb423ae5997
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-08-05 15:17:34 +0900

    graceful restart用の設定を追加

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -12,8 +12,17 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) &&
-    ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
 end
 
 after_fork do |server, worker|
