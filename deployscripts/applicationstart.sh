aa3b16d90d41af400a6ae98b08102564f87e8997
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-08-05 15:19:28 +0900

    unicornのmasterプロセスが存在するときはgraceful restartするためにUSR2シグナルを送るように変更

--- deployscripts/applicationstart.sh
+++ deployscripts/applicationstart.sh
@@ -6,10 +6,9 @@ readonly UNICORN_MASTER_ID=`ps aux | grep unicorn_rails\ master | grep -v grep |
 cd ${PRODUCTION_DIR}
 
 if [ -n "$UNICORN_MASTER_ID" ]; then
-  kill -9 $UNICORN_MASTER_ID
-  /usr/local/rbenv/shims/bundle exec unicorn_rails -c ${PRODUCTION_DIR}/config/unicorn.rb -D -E production > /tmp/test.log
+  kill -s USR2 $UNICORN_MASTER_ID
 else
-  /usr/local/rbenv/shims/bundle exec unicorn_rails -c ${PRODUCTION_DIR}/config/unicorn.rb -D -E production > /tmp/test.log
+  /usr/local/rbenv/shims/bundle exec unicorn_rails -c ${PRODUCTION_DIR}/config/unicorn.rb -D -E production
 fi
 
 readonly NGINX_MASTER_ID=`ps aux | grep nginx:\ master | grep -v grep | awk '{ print $2 }'`
