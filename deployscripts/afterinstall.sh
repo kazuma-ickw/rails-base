adbd14347a30e07ed70c1c9f0fa738f9bf85d6c0
Kazuma Ichikawa <b.g.ibx.leo10m@gmail.com>
2017-08-06 19:35:38 +0900

    不要なenv出力を削除

--- deployscripts/afterinstall.sh
+++ deployscripts/afterinstall.sh
@@ -2,8 +2,6 @@
 
 readonly PRODUCTION_DIR=/srv/app
 
-env > /tmp/home
-
 cd ${PRODUCTION_DIR}
 /usr/local/rbenv/shims/bundle install --path=vendor/bundle
 /usr/local/rbenv/shims/bundle exec rake assets:precompile RAILS_ENV=production
